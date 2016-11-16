b80e5081d19a9c76c9a956f70d6cd493e97017bb
Chris Isler <christopherisler1@gmail.com>
2016-11-15 23:47:03 -0500

    sick new changes

---
+++ Conventions.md
@@ -0,0 +1,19 @@
+Conventions
+===========
+
+* Vertical space is a lovely thing
+* HTML/CSS classes prepended with `js-` are for JS, not CSS.
+
+HTML
+----
+
+* Separate multiple classes with two spaces (unless adding classes with JS)
+
+Javascript
+----------
+
+* Member names: `camelCase`
+* Private member names: `_camelCase`
+* One class per file
+* Variable names of HTML elements selected with jquery like must be suffixed with Elem. Example: `const boxWrapElem = $('.box-wrap');`
+
--- src/main.js
+++ src/main.js
@@ -11,22 +11,63 @@ process.on('uncaughtException', (error) =>
 });
 
 const {app, BrowserWindow} = require('electron');
-const Path = require('path');
-const Url = require('url');
 
-// Global reference to the (main) window object).
-// Avoids automatic JS object garbage collection.
-let win;
+// Global reference to the (main) window object.
+let mainWindow = null;
+
+// Currently, there is no support for running multiple instance of the app at
+// the same time.
+let allowMultipleInstances = false;
+
+/*
+const appAlreadyRunning = app.makeSingleInstance(() =>
+{
+    // What to do when someone tries to run a second instance of the app.
+    if (allowMultipleInstances)
+    {
+        if (mainWindow.isMinimized()) { mainWindow.restore(); }
+        mainWindow.focus();
+    }
+    else
+    {
+        app.quit();
+    }
+});
+
+// Currently, there is no support for running multiple instance of the app at
+// the same time.
+if (appAlreadyRunning)
+{
+    app.relaunch();
+}
+*/
+
+const WINDOW_MIN_WIDTH = 800;
+const WINDOW_MIN_HEIGHT = 600;
+const WINDOW_DEFAULT_WIDTH = 1200;
+const WINDOW_DEFAULT_HEIGHT = 900;
 
 /**
  * Create the browser (main) window.
  */
 function createWindow()
 {
-    win = new BrowserWindow({ width: 1200, height: 900 });
+    const Url = require('url');
+    const Path = require('path');
+
+    const windowOptions =
+    {
+        minWidth: WINDOW_MIN_WIDTH,   // Not working - unless this is min starting value.
+        minHeight: WINDOW_MIN_HEIGHT, // Not working - unless this is min starting value.
+        width: WINDOW_DEFAULT_WIDTH,
+        height: WINDOW_DEFAULT_HEIGHT,
+        icon: './images/ErxiDesk-Icon-1.0.png'
+    };
+
+    mainWindow = new BrowserWindow(windowOptions);
 
     // Load index.html
-    win.loadURL(Url.format(
+    mainWindow.loadURL(Url.format(
     {
         pathname: Path.join(__dirname, 'index.html'),
         protocol: 'file:',
@@ -34,16 +75,16 @@ function createWindow()
     }));
 
     // Automatically open Chromium DevTools.
-    // win.webContents.openDevTools();
+    // mainWindow.webContents.openDevTools();
 
     // When the window is closed, dereference the window
     // object for garbage collection
-    win.on('closed', () =>
+    mainWindow.on('closed', () =>
     {
-        win = null;
+        mainWindow = null;
     });
 }
 
-// WHen the app is ready, display the window.
+// When the app is ready, display the window.
 app.on('ready', createWindow);
 
